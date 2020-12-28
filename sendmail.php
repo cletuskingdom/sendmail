b99a6dac036faefe4b686fe000429489c048edb1
cletuskingdom <cletuskingdom@gmail.com>
2020-12-28 13:41:34 +0100

    Completely edited the sendmail.php file.

--- sendmail.php
+++ sendmail.php
@@ -14,17 +14,17 @@
         $email = test_input($_POST['email']);
         $message = test_input($_POST['message']);
 
-        if (empty($email) or empty($phone) or empty($email) or empty($gender) or empty($location) or empty($message)){
-          $response = array('status' => 0, 'message' => "Du måste fylla alla ingångar!");
+        if (empty($email) or empty($message)){
+          $response = ['status' => 0, 'message' => "Sorry, please fill all the fields!"];
         }else{
-          require_once 'fetch.php';
-          $bookService = $insertData->bookServiceNow($name, $phone, $email, $gender, $location, $message, $bookTime);
-          $msg = "namn : ".$name."\n Telefon : ".$phone."\n E-post : ".$email."\n Kön : ".$gender."\n Plats : ".$location."\n Meddelande : ".$message."\n Datum : ".$bookTime;
-          $to ="cletuskingdom@gmail.com";
-          if(mail($to,$subject,$msg)){
-            $response = ['status' => 1, 'message' => "Tjänsten bokad."];
+          $to = $email;
+          $subject = "Testing mail!!!";
+          $msg = $message;
+          
+          if(mail($to, $subject, $msg)){
+            $response = ['status' => 1, 'message' => "Mail sent."];
           }else {
-            $response = ['status' => 0, 'message' => "Tjänsten kunde inte bokas, kanske ett nätverksproblem."];
+            $response = ['status' => 0, 'message' => "The mail wasn't sent."];
           }
         }
       }

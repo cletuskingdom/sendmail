cf6ff6e1eb09dc719028f1131c88f9c06cfddafa
cletuskingdom <cletuskingdom@gmail.com>
2020-12-28 13:26:40 +0100

    Edited the index.php file.

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Send mail</title>
+	</head>
+
+	<body>
+		<form id="sendmail" method="POST">
+			<input type="email" name="email" placeholder="Enter desired email"> <br><br>
+			<textarea placeholder="Write your message"></textarea> <br><br><br>
+			<button type="submit">Send mail</button>
+		</form>
+	</body>
+</html>

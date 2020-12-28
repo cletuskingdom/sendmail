354a035766457264c1b6d47e4f7e7076037a7fc4
cletuskingdom <cletuskingdom@gmail.com>
2020-12-28 13:29:34 +0100

    Edited the index.php file again.

--- index.php
+++ index.php
@@ -10,5 +10,27 @@
 			<textarea placeholder="Write your message"></textarea> <br><br><br>
 			<button type="submit">Send mail</button>
 		</form>
+
+		<script type="text/javascript">
+			$('#bookS').submit(function(e){
+			  (e).preventDefault();
+
+			    $.ajax({
+			      url: 'sendmail.php?task=mail',
+			      type: 'POST',
+			      dataType: 'json',
+			      data: $(this).serialize()
+			    })
+			    .done(function(response){
+			      if(response.status == 1){
+			        alert(response.message);
+			        location.href = './';
+			      }
+			      else{
+			        alert(response.message);
+			      }
+			    });
+			});
+		</script>
 	</body>
 </html>

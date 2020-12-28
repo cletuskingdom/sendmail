36f36967faffaaab555251a7842e40736af4d90e
cletuskingdom <cletuskingdom@gmail.com>
2020-12-28 14:42:56 +0100

    Added jQuery cdn

--- index.php
+++ index.php
@@ -11,6 +11,10 @@
 			<button type="submit">Send mail</button>
 		</form>
 
+		<script
+		  src="https://code.jquery.com/jquery-3.5.1.min.js"
+		  integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+		  crossorigin="anonymous"></script>
 		<script>
 			$('#sendmail').submit(function(e){
 			  (e).preventDefault();
